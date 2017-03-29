1c9f9c9322a5642fc5e4c6ff748330411fd95fda
jianpengh <jianpengh@tujia.com>
2017-03-29 10:07:37 +0800

    Add project files.

---
+++ Log4net.Mysql/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension = "config", Watch = true)]
+namespace Log4net.Mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = log4net.LogManager.GetLogger("MyLogger");
+
+            log.Info("hello world! Info");
+            log.Debug("hello world! Debug");
+
+
+            Console.ReadKey();
+        }
+    }
+}
